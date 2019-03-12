e9d4f3aa8a59fcbd880138429651009b11d1d547
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2019-03-12 20:01:33 +0000

    Initialise project

---
+++ src/main/java/com/joshuastringfellow/Norway/EV/Stats/Scrapping/API/NorwayEvStatsScrappingApiApplication.java
@@ -0,0 +1,13 @@
+package com.joshuastringfellow.Norway.EV.Stats.Scrapping.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorwayEvStatsScrappingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorwayEvStatsScrappingApiApplication.class, args);
+	}
+
+}
