2ed8458a11c62e27466fadab3a0f07754a3f72f2
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2019-04-20 23:28:00 +0100

    Rename main application test class

--- src/test/java/com/joshuastringfellow/statsscraper/ApplicationTests.java
+++ src/test/java/com/joshuastringfellow/statsscraper/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.joshuastringfellow.Norway.EV.Stats.Scrapping.API;
+package com.joshuastringfellow.statsscraper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NorwayEvStatsScrappingApiApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
